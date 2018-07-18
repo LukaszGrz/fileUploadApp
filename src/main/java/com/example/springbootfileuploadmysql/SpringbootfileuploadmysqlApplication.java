e4448cc053ba4d042ba50799fa1358f4ff13f891
grzelakl <lukasz.grzelak@contractors.roche.com>
2018-07-18 10:42:31 +0200

    exceptiony zrobione

--- src/main/java/com/example/springbootfileuploadmysql/SpringbootfileuploadmysqlApplication.java
+++ src/main/java/com/example/springbootfileuploadmysql/SpringbootfileuploadmysqlApplication.java
@@ -1,9 +1,14 @@
 package com.example.springbootfileuploadmysql;
 
+import com.example.springbootfileuploadmysql.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class SpringbootfileuploadmysqlApplication {
 
 	public static void main(String[] args) {
